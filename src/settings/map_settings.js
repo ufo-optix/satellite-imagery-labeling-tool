ae2e7a3b6bcf4c09a1bf81eb5461935503d2c660
markmnl <markmnl@outlook.com>
2023-08-17 11:35:08 +0800

    mv index.html to src/

--- src/settings/map_settings.js
+++ src/settings/map_settings.js
@@ -5,19 +5,19 @@ export let mapSettings = {
     //Optional. Azure Maps authentication information.
 	azureMapsAuth: {
 		
-		//Option 1: Use Azure Active Directory authentication for secure access to Azure Maps.
-		authType: "anonymous",
-		clientId: "d069e722-70c3-4dd6-8532-a6f4b18c9bfb", //Your Azure Maps client id for accessing your Azure Maps account.
-		getToken: function (resolve, reject, map) {
-			//URL to your authentication service that retrieves an Azure Active Directory Token.
-			var tokenServiceUrl = "https://spatial-annotation-tool-maps-auth2.azurewebsites.net/api/GetAzureMapsToken";
+		// //Option 1: Use Azure Active Directory authentication for secure access to Azure Maps.
+		// authType: "anonymous",
+		// clientId: "d069e722-70c3-4dd6-8532-a6f4b18c9bfb", //Your Azure Maps client id for accessing your Azure Maps account.
+		// getToken: function (resolve, reject, map) {
+		// 	//URL to your authentication service that retrieves an Azure Active Directory Token.
+		// 	var tokenServiceUrl = "https://spatial-annotation-tool-maps-auth2.azurewebsites.net/api/GetAzureMapsToken";
 
-			fetch(tokenServiceUrl).then(r => r.text()).then(token => resolve(token));
-		}
+		// 	fetch(tokenServiceUrl).then(r => r.text()).then(token => resolve(token));
+		// }
 
 		//Option 2: Use an Azure Maps key. Get an Azure Maps key at https://azure.com/maps. NOTE: The primary key should be used as the key.
-		/*authType: "subscriptionKey",
-		subscriptionKey: "<Your Azure Maps Key>"*/
+		authType: "subscriptionKey",
+		subscriptionKey: "<Your Azure Maps Key>" // https://github.com/microsoft/satellite-imagery-labeling-tool/issues/15
 	},
 
     //A URL to a CORs enabled proxy service that can be leveraged for cross domain requests. Cross domain URL would be appended to this proxy service URL. 
